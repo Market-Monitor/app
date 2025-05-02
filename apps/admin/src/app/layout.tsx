3bb359f895f0342f59137d431ffeea331b25a0df
Joshue Abance <iamcoderx@gmail.com>
2025-05-02 22:01:24 +0800

    init admin web app

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+import { Providers } from "@/components/providers";
+import "@mm-app/ui/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen overflow-x-hidden`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
