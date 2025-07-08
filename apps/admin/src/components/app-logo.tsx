e2c5fcede8aae186bbc0364142d4e53895878ea2
Joshue Abance <iamcoderx@gmail.com>
2025-07-08 17:31:36 +0800

    admin: adjustments to app logo styling

--- apps/admin/src/components/app-logo.tsx
+++ apps/admin/src/components/app-logo.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import LogoPNG from "@/assets/logo.png";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@mm-app/ui/components/sidebar";
 import { cn } from "@mm-app/ui/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,24 +16,35 @@ export default function AppLogo(props: {
   textClassName?: string;
 }) {
   return (
-    <div className={cn("flex items-center justify-center", props.className)}>
-      <Link href="/" className="flex flex-col items-center justify-center">
-        <Image
-          src={LogoPNG}
-          className={cn("w-24 h-24", props.imageClassName)}
-          alt="AgriTrakPH Logo"
-        />
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton size={"lg"} className="h-[auto] py-4" asChild>
+          <Link href={"/"}>
+            <div
+              className={cn(
+                "flex items-center justify-center",
+                props.className,
+              )}
+            >
+              <Image
+                src={LogoPNG}
+                className={cn("size-20", props.imageClassName)}
+                alt="AgriTrakPH Logo"
+              />
 
-        <h1
-          className={cn(
-            "font-black text-lg dark:text-gray-200 leading-none text-center leading-normal",
-            props.textClassName,
-          )}
-        >
-          <span className="text-primary">Agri</span>TrakPH <br />
-          <strong>(ADMIN)</strong>
-        </h1>
-      </Link>
-    </div>
+              <h1
+                className={cn(
+                  "font-black text-lg dark:text-gray-200 leading-none text-center leading-normal",
+                  props.textClassName,
+                )}
+              >
+                <span className="text-primary">Agri</span>TrakPH <br />
+                <span className="text-xs font-bold">ADMIN</span>
+              </h1>
+            </div>
+          </Link>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    </SidebarMenu>
   );
 }
